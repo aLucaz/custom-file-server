2f6421d2ef4a33f92223a2fda8e17bef3f9d4325
Jhamir Arturo Lucas Ramos <jlucas@proveedor.intercorp.com.pe>
2022-09-27 18:59:06 -0500

    using main arguments

--- client/main.go
+++ client/main.go
@@ -23,12 +23,11 @@ func main() {
 	decoded := util.DecodeBase64Str(constant.CLIENT_BANNER)
 	util.WriteBanner(decoded)
 	util.WriteMsgLog(constant.INFO, "Starting client...")
-	//arguments := os.Args
-	arguments := [2]string{"receive", "channel-1"}
-	if arguments[0] == constant.RECEIVE_MODE {
+	arguments := os.Args
+	if arguments[1] == constant.RECEIVE_MODE {
 		util.WriteMsgLog(constant.INFO, "Reveiver mode activated")
 		server, err := net.Listen(constant.SERVER_TYPE, constant.CLIENT_HOST+":")
-		request := service.CreateClientRegistrationRequest(server.Addr().String(), arguments[1])
+		request := service.CreateClientRegistrationRequest(server.Addr().String(), arguments[2])
 		_, err = connection.Write(request)
 		if err != nil {
 			util.WriteMsgLog(constant.ERROR, err.Error())
